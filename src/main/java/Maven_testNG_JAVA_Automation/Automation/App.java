c7fa000ad99ecc272a4eeecbb0531fb11bd69575
rk57475 <rraj57475@gmail.com>
2022-05-27 00:26:24 +0530

    Maven Framework

---
+++ src/main/java/Maven_testNG_JAVA_Automation/Automation/App.java
@@ -0,0 +1,13 @@
+package Maven_testNG_JAVA_Automation.Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Maven_testNG_JAVA_Automation/Automation/AppTest.java
@@ -0,0 +1,20 @@
+package Maven_testNG_JAVA_Automation.Automation;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
